d197fbfde03ac520680de62fe8ef10c3d6d70f98
织梦行云之唯心所识 <313097973@qq.com>
2022-08-31 21:42:02 +0800

    格式化代码

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,7 +6,7 @@ This file is part of FirecmsExt utils.
 @link     https://www.klmis.cn
 @document https://www.klmis.cn
 @contact  zhimengxingyun@klmis.cn
-@license  https://gitee.com/firecms-ext/utils/blob/master/LICENSE
+@license  https://github.com/firecms-ext/utils/blob/master/LICENSE
 EOF;
 
 return (new PhpCsFixer\Config())
--- tests/Cases/FunctionTest.php
+++ tests/Cases/FunctionTest.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 /**
- * This file is part of FirecmsExt Demo.
+ * This file is part of FirecmsExt utils.
  *
  * @link     https://www.klmis.cn
  * @document https://www.klmis.cn
  * @contact  zhimengxingyun@klmis.cn
- * @license  https://gitee.com/firecms-ext/demo/blob/master/LICENSE
+ * @license  https://github.com/firecms-ext/utils/blob/master/LICENSE
  */
 namespace HyperfTest\Cases;
 
