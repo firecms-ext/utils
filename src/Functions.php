ea861e0a47f1a15727cb2729cc1e5062f945036d
织梦行云之唯心所识 <313097973@qq.com>
2022-09-23 15:38:27 +0800

    优化

--- src/Functions.php
+++ src/Functions.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
  * @license  https://github.com/firecms-ext/utils/blob/master/LICENSE
  */
 
-use Hyperf\Context\Context;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
 use Hyperf\Redis\Redis;
