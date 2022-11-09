2e7526d99688d753e02b22cb088815543ddd548c
ella123 <313097973@qq.com>
2022-11-09 15:42:21 +0800

    添加 RPC 基类服务

---
+++ src/JsonRpc/Provider/AbstractService.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of FirecmsExt utils.
+ *
+ * @link     https://www.klmis.cn
+ * @document https://www.klmis.cn
+ * @contact  zhimengxingyun@klmis.cn
+ * @license  https://github.com/firecms-ext/utils/blob/master/LICENSE
+ */
+namespace FirecmsExt\Utils\JsonRpc\Provider;
+
+use FirecmsExt\Utils\Service\BaseService;
+use FirecmsExt\Utils\Service\BaseServiceInterface;
+
+abstract class AbstractService extends BaseService implements BaseServiceInterface
+{
+}
