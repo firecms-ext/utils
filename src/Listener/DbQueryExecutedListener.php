f7f13450a269cc19a28bf9f3eab1deea672eafc7
ella123 <313097973@qq.com>
2022-11-27 14:06:54 +0800

    数据查询 监听

---
+++ src/Listener/DbQueryExecutedListener.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of FirecmsLaw Crawl.
+ *
+ * @link     https://www.klmis.cn
+ * @document https://www.klmis.cn
+ * @contact  zhimengxingyun@klmis.cn
+ */
+namespace FirecmsExt\Utils\Listener;
+
+use Hyperf\Database\Events\QueryExecuted;
+use Hyperf\Event\Annotation\Listener;
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Logger\LoggerFactory;
+use Hyperf\Utils\Arr;
+use Hyperf\Utils\Str;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+#[Listener]
+class DbQueryExecutedListener implements ListenerInterface
+{
+    private LoggerInterface $logger;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->logger = $container->get(LoggerFactory::class)
+            ->get('sql');
+    }
+
+    public function listen(): array
+    {
+        return [
+            QueryExecuted::class,
+        ];
+    }
+
+    public function process(object $event): void
+    {
+        if ($event instanceof QueryExecuted) {
+            $sql = $event->sql;
+            if (! Arr::isAssoc($event->bindings)) {
+                foreach ($event->bindings as $key => $value) {
+                    $sql = Str::replaceFirst('?', "'{$value}'", $sql);
+                }
+            }
+            if (env('APP_ENV') == 'dev') {
+                ddd($sql);
+            }
+            $this->logger->info(sprintf('[%s] %s', $event->time, $sql));
+        }
+    }
+}
