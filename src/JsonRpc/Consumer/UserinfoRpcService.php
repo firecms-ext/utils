ed79da866f9b491ffd43b99403eb5a03b6f2942c
ella123 <313097973@qq.com>
2022-12-08 15:16:30 +0800

    添加 用户信息 接口

--- src/JsonRpc/Consumer/UserinfoRpcService.php
+++ src/JsonRpc/Consumer/UserinfoRpcService.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 /**
- * This file is part of FirecmsLaw Admin-Http.
+ * This file is part of FirecmsExt utils.
  *
  * @link     https://www.klmis.cn
  * @document https://www.klmis.cn
  * @contact  zhimengxingyun@klmis.cn
+ * @license  https://github.com/firecms-ext/utils/blob/master/LICENSE
  */
-
 namespace FirecmsExt\Utils\JsonRpc\Consumer;
 
 class UserinfoRpcService extends AbstractServiceClient implements UserinfoRpcServiceInterface
